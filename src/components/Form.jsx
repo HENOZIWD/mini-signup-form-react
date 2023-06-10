d9de0088adbd686cd3f15dcf209d514e58c06181
Donghyeon Lee <ehdgus001928@gmail.com>
2023-06-10 18:08:26 +0900

    refactor: Refactor code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,123 +1,56 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useState } from 'react'
 import FormInput from './FormInput'
 import { FormContext } from '../App'
 
+const REGEXP = {
+    ID: /^[a-z0-9_-]{5,20}$/,
+    PW: /^[A-Za-z0-9]{8,16}$/,
+}
+
 const Form = ({ showModal }) => {
-    const { formData, setFormData } = useContext(FormContext)
-    const [idMsg, setIdMsg] = useState('')
-    const [pwMsg, setPwMsg] = useState('')
-    const [confirmPwMsg, setConfirmPwMsg] = useState('')
-    const idInputRef = useRef(null)
-    const pwInputRef = useRef(null)
-    const confirmPwInputRef = useRef(null)
+    const { formData } = useContext(FormContext)
+    const [formState, setFormState] = useState({
+        id: 'INITIAL',
+        pw: 'INITIAL',
+        confirmPw: 'INITIAL',
+    })
 
-    useEffect(() => {
-        if (idInputRef.current) {
-            idInputRef.current.focus()
-        }
-    }, [])
-
-    const ERR_MSG = {
-        EMPTY: '필수 정보입니다.',
-        INVALID_ID:
-            '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
-        INVALID_PW: '8~16자 영문 대 소문자, 숫자를 사용하세요.',
-        INVALID_CONFIRM_PW: '비밀번호가 일치하지 않습니다.',
-    }
-
-    const REGEXP = {
-        ID: /^[a-z0-9_-]{5,20}$/,
-        PW: /^[A-Za-z0-9]{8,16}$/,
-    }
-
-    const inputCheck = (target, value) => {
+    const inputCheck = (target) => {
+        let result
+        const value = formData[target]
         if (value === '') {
-            return 'EMPTY'
+            result = 'EMPTY'
+        } else {
+            switch (target) {
+                case 'id':
+                    result = REGEXP.ID.test(value) ? true : 'INVALID_ID'
+                    break
+
+                case 'pw':
+                    result = REGEXP.PW.test(value) ? true : 'INVALID_PW'
+                    inputCheck('confirmPw')
+                    break
+
+                case 'confirmPw':
+                    result = value === formData.pw ? true : 'INVALID_CONFIRM_PW'
+                    break
+
+                default:
+                    break
+            }
         }
-        switch (target) {
-            case 'id':
-                return REGEXP.ID.test(value) ? true : 'INVALID_ID'
 
-            case 'pw':
-                return REGEXP.PW.test(value) ? true : 'INVALID_PW'
-
-            case 'confirmPw':
-                return value === formData.pw ? true : 'INVALID_CONFIRM_PW'
-
-            default:
-                break
-        }
-    }
-
-    const validation = (target) => {
-        switch (target) {
-            case 'id':
-                const idCheckResult = inputCheck(target, formData.id)
-                if (idCheckResult === true) {
-                    setIdMsg('')
-                    if (idInputRef.current) {
-                        idInputRef.current.classList.remove('border-red-600')
-                    }
-                } else {
-                    setIdMsg(ERR_MSG[idCheckResult])
-                    if (idInputRef.current) {
-                        idInputRef.current.classList.add('border-red-600')
-                    }
-                }
-                return idCheckResult === true
-
-            case 'pw':
-                const pwCheckResult = inputCheck(target, formData.pw)
-                if (pwCheckResult === true) {
-                    setPwMsg('')
-                    if (pwInputRef.current) {
-                        pwInputRef.current.classList.remove('border-red-600')
-                    }
-                } else {
-                    setPwMsg(ERR_MSG[pwCheckResult])
-                    if (pwInputRef.current) {
-                        pwInputRef.current.classList.add('border-red-600')
-                    }
-                }
-                return pwCheckResult === true
-
-            case 'confirmPw':
-                const confirmPwCheckResult = inputCheck(
-                    target,
-                    formData.confirmPw
-                )
-                if (confirmPwCheckResult === true) {
-                    setConfirmPwMsg('')
-                    if (confirmPwInputRef.current) {
-                        confirmPwInputRef.current.classList.remove(
-                            'border-red-600'
-                        )
-                    }
-                } else {
-                    setConfirmPwMsg(ERR_MSG[confirmPwCheckResult])
-                    if (confirmPwInputRef.current) {
-                        confirmPwInputRef.current.classList.add(
-                            'border-red-600'
-                        )
-                    }
-                }
-                return confirmPwCheckResult === true
-
-            default:
-                break
-        }
+        setFormState((prev) => ({ ...prev, [target]: result }))
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        const [idStatus, pwStatus, confirmPwStatus] = [
-            validation('id'),
-            validation('pw'),
-            validation('confirmPw'),
-        ]
+        Object.keys(formState).forEach((target) => {
+            inputCheck(target)
+        })
 
-        if (idStatus && pwStatus && confirmPwStatus) {
+        if (Object.values(formState).every((value) => value === true)) {
             showModal()
         }
     }
@@ -130,50 +63,35 @@ const Form = ({ showModal }) => {
             onSubmit={handleSubmit}
         >
             <FormInput
-                ref={idInputRef}
                 id={'id'}
                 label={'아이디'}
-                msg={idMsg}
-                validation={validation}
-                handleChange={(e) =>
-                    setFormData({ ...formData, id: e.target.value })
-                }
+                validation={inputCheck}
+                formState={formState.id}
                 inputProps={{
                     type: 'text',
                     placeholder: '아이디를 입력해주세요',
-                    value: formData.id,
                 }}
             />
             <FormInput
-                ref={pwInputRef}
                 id={'pw'}
                 label={'비밀번호'}
-                msg={pwMsg}
-                validation={validation}
-                handleChange={(e) =>
-                    setFormData({ ...formData, pw: e.target.value })
-                }
+                validation={inputCheck}
+                formState={formState.pw}
                 inputProps={{
                     type: 'password',
                     placeholder: '비밀번호를 입력해주세요',
                     autoComplete: 'off',
-                    value: formData.pw,
                 }}
             />
             <FormInput
-                ref={confirmPwInputRef}
                 id={'confirmPw'}
                 label={'비밀번호 확인'}
-                msg={confirmPwMsg}
-                validation={validation}
-                handleChange={(e) =>
-                    setFormData({ ...formData, confirmPw: e.target.value })
-                }
+                validation={inputCheck}
+                formState={formState.confirmPw}
                 inputProps={{
                     type: 'password',
                     placeholder: '비밀번호 확인을 입력해주세요',
                     autoComplete: 'off',
-                    value: formData.confirmPw,
                 }}
             />
             <div className="flex items-center justify-center">
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,9 +1,24 @@
-import { forwardRef } from 'react'
+import { useContext, useEffect, useRef } from 'react'
+import { FormContext } from '../App'
 
-const FormInput = forwardRef(function FormInput(
-    { id, label, msg, validation, handleChange, inputProps },
-    inputRef
-) {
+const ERR_MSG = {
+    EMPTY: '필수 정보입니다.',
+    INVALID_ID:
+        '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
+    INVALID_PW: '8~16자 영문 대 소문자, 숫자를 사용하세요.',
+    INVALID_CONFIRM_PW: '비밀번호가 일치하지 않습니다.',
+}
+
+const FormInput = ({ id, label, validation, formState, inputProps }) => {
+    const { setFormData } = useContext(FormContext)
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        if (id === 'id' && inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [])
+
     return (
         <div className="mb-4">
             <label
@@ -15,14 +30,28 @@ const FormInput = forwardRef(function FormInput(
             <input
                 ref={inputRef}
                 id={id}
-                className="shadow border rounded w-full py-2 px-3 text-gray-700"
+                className={
+                    'shadow border rounded w-full py-2 px-3 text-gray-700' +
+                    (formState === true || formState === 'INITIAL'
+                        ? ''
+                        : ' border-red-600')
+                }
                 onBlur={() => validation(id)}
-                onChange={handleChange}
+                onChange={(e) => {
+                    setFormData((prev) => ({
+                        ...prev,
+                        [id]: e.target.value,
+                    }))
+                }}
                 {...inputProps}
             />
-            <div className="mt-1 mb-3 text-xs text-red-500">{msg}</div>
+            <div className="mt-1 mb-3 text-xs text-red-500">
+                {formState === true || formState === 'INITIAL'
+                    ? ''
+                    : ERR_MSG[formState]}
+            </div>
         </div>
     )
-})
+}
 
 export default FormInput
